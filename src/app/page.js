a63d88223eaff014edfd19094e9cf955134afb9f
tluo3032 <tiantian.luo@uzh.ch>
2023-03-16 13:50:00 +0100

    Add Waffle Chart Layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import styles from './page.module.css'
 import XAInavbar from "@/components/Navbar";
 import Head from "next/head";
 import Sidebar  from "../components/Sidebar";
+import chart from "../components/chart.jpg";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,8 +18,26 @@ export default function Home() {
               <div>
                   <XAInavbar/>
               </div>
-              <div>
-                  <Sidebar/>
+              <div className={styles.waffleContainer}>
+                  <div>
+                      <Sidebar/>
+                  </div>
+                  <div className={styles.wafflebox}>
+                      <div>1</div>
+                      <div>2</div>
+                      <div>3</div>
+                      <div>4</div>
+                      <div>5</div>
+                      <div>6</div>
+                      <div>7</div>
+                      <div>8</div>
+                      <div>9</div>
+                      <div>10</div>
+                  </div>
+                  <div>
+                      <Image src={chart}
+                             width={260}/>
+                  </div>
               </div>
               <footer className={styles.footer}>
                   <p>2023 XAI for Rank Visualization</p>
